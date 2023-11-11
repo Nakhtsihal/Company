05be9eebd68273d30481e78217647445eb97643c
Nakhtsihal <nakhtsihal@yandex.ru>
2023-11-11 18:39:10 +0200

    Homework

--- src/telran/interview/InterviewTasks.java
+++ src/telran/interview/InterviewTasks.java
@@ -1,39 +1,78 @@
 package telran.interview;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class InterviewTasks {
-/**
- * 
- * @param ar array of integer numbers
- * @param sum integer number
- * @return true if the given array contains two numbers, the sum of which equals the given sum value
- */
-	public static boolean isSum2(int [] ar, int sum) {
-	//TODO  should be done with O[N]
+	/*
+	 *
+	 * @param ar array of integer numbers
+	 * @param sum integer number
+	 * @return true if the given array contains two numbers, the sum of which equals the given sum value
+	 */
+
+	public static void main(String[] args) {
+		int [] ar1 = {1000, 300, -200, 20, 40, -10};
+
+		isSum2(ar1, 800);
+
+	}
+
+	public static boolean isSum2(int[] ar, int sum) {
+		HashSet<Integer> seenNumbers = new HashSet<>();
+		for (int num : ar) {
+			int complement = sum - num;
+
+			if (seenNumbers.contains(complement)) {
+				return true;
+			}
+
+			seenNumbers.add(num);
+		}
+
 		return false;
-}
-	/**
-	 * 
+	}
+
+
+
+	/*
 	 * @param ar array of integer numbers
 	 * @return maximal positive number having negative number with the same absolute value
 	 */
-	public static int getMaxPositiveWithNegativeValue (int ar[]) {
-		//TODO should be done with O[N]
-		return -1;
+	public static int getMaxPositiveWithNegativeValue(int ar[]) {
+		int numb = Integer.MIN_VALUE;
+		int testNum = 0;
+		HashSet<Integer> seenNumbers = new HashSet<>();
+		for(int num: ar){
+			testNum = num * - 1;
+			if(seenNumbers.contains(testNum) && testNum > numb) {
+				numb = testNum;
+		}
+			if(num < 0 && num > numb){
+				numb = num;
+			}
+			seenNumbers.add(num);
+		}
+		return numb;
 	}
-	
-	/**
-	 * 
+
+	/*
 	 * @param strings - array of strings
 	 * @return Map where key - string, value - number of occurrences in the array
 	 */
 	public static HashMap<String, Long> getMapOccurrences(String[] strings) {
-		//TODO should be done with complexity O[N]
-		//Optionally, additional bonus if you apply the method "merge" of the interface Map
-		//try to understand this method https://docs.oracle.com/javase/8/docs/api/java/util/Map.html#merge-K-V-java.util.function.BiFunction- 
-		//it uses Functional interface BiFunction
-		return null;
+		HashMap<String, Long> map = new HashMap<>();
+		for(String s: strings){
+			boolean res = map.containsKey(s);
+			if(res == false){
+				map.put(s,1L);
+			}
+			else{
+				Long value = map.get(s);
+				value++;
+				map.replace(s,value);
+			}
+		}
+		return map;
 	}
-	
 }
