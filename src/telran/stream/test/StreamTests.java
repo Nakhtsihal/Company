c3d75701aadf0291ed4870cc95eead065eb2eb59
Nakhtsihal <nakhtsihal@yandex.ru>
2023-11-14 11:40:21 +0200

    streams:conversion and grouping

--- src/telran/stream/test/StreamTests.java
+++ src/telran/stream/test/StreamTests.java
@@ -2,19 +2,23 @@ package telran.stream.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class StreamTests {
+    int[] arr = {10, 13, 8, 7, 3, 5 , 6};
 
     @Test
     @Disabled
     void arrayStreamTest() {
         //computing sum of the even numbers
-        int[] arr = {10, 13, 8, 7, 3, 5 , 6};
         int [] empty = {};
         assertEquals(24, Arrays.stream(arr)
                 .filter(n -> n % 2 == 0)
@@ -27,11 +31,63 @@ class StreamTests {
                 .max().orElse(0));
     }
     @Test
+    @Disabled
     void displaySportloto() {
         Random gen = new Random();
         gen.ints(7, 1, 50)
                 .distinct()
                 .forEach(n -> System.out.print(n + " "));
     }
-
+    @Test
+    @Disabled
+    /*
+    void evenOddGrouping(){
+        Arrays.stream(arr).boxed()
+                .collect(Collectors.groupingBy(n -> {
+                    String res = "";
+                    if (n % 2 == 0) {
+                        res = "even";
+                    } else {
+                        res = "odd";
+                    }
+                    return res;
+                }));
+    }
+    */
+    void evenOddGrouping(){
+        Map<String, List<Integer>> mapOddEven = Arrays.stream(arr).boxed()
+                .collect(Collectors.groupingBy(n -> n % 2 == 0 ? "even" : "odd"));
+        System.out.println(mapOddEven);
+    }
+    @Test
+    @Disabled
+    void displayOccurrenceSorted () {
+        String[] strings = {"lpm", "y", "a", "lpm", "aa", "yy", "yy", "aa", "lpm"};
+        /*
+        output:
+        lpm => 3
+        aa => 2
+        yy => 2
+        a => 1
+        y => 1
+         */
+        Map<String, Long> occurrencesMap = Arrays.stream(strings)
+                .collect(Collectors.groupingBy(s -> s, Collectors.counting()));
+                occurrencesMap.entrySet().stream().sorted((e1,e2) -> {
+            int res = Long.compare(e2.getValue(), e1.getValue());
+            if (res == 0) {
+                res = e1.getKey().compareTo(e2.getKey());
+            }
+            return res;
+        })
+                .forEach(e -> System.out.printf("%s => %d\n", e.getKey(), e.getValue()));
+    }
+    @Test
+    void stringSteam(){
+        String string = "Hello";
+        // output: h, e, l, l, o
+        // string.chars().forEach(c -> System.out.printf("%c,", c));
+        string.chars().mapToObj(c -> "" + (char)c) // conversion to Stream<String>
+                .forEach(s -> System.out.print(s + ","));
+    }
 }
