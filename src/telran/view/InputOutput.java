fce118b00564b0d2022c91f089673d96d901ab2c
Nakhtsihal <nakhtsihal@yandex.ru>
2023-11-21 09:05:59 +0200

    class adds

--- src/telran/view/InputOutput.java
+++ src/telran/view/InputOutput.java
@@ -1,28 +1,92 @@
 package telran.view;
-
-
+import java.time.LocalDate;
+import java.util.Set;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public interface InputOutput {
     String readString(String prompt);
+
     void write(String str);
-    default void writeLine(String str){
-        write (str + "\n");
+
+    default void writeLine(Object obj) {
+        write(obj.toString() + "\n");
     }
-    default <T> T readObject(String promt, String errorPromt, Function<String, T> mapper){
+
+    default <T> T readObject(String promt, String errorPromt, Function<String, T> mapper) {
         boolean running = false;
         T res = null;
-        do{
+        do {
             running = false;
-            try{
+            try {
                 String str = readString(promt);
                 res = mapper.apply(str);
-            }catch (Exception e){
+            } catch (Exception e) {
                 running = true;
                 writeLine(errorPromt + ": " + e.getMessage());
             }
-        }while(running);
-        return null;
+        } while (running);
+        return res;
     }
 
+    default int readInt(String promt, String errorPrompt) {
+        return readObject(promt, errorPrompt, Integer::parseInt);
+    }
+
+    default int readInt(String promt, String errorPrompt, int min, int max) {
+        return readObject(String.format("%s", "[%d-%d]", promt, min, max), errorPrompt, str -> {
+            int num = Integer.parseInt(str);
+            if (num < min || num > max) {
+                throw new RuntimeException(String.format("must be in the range [%d-%d]", min, max));
+            }
+            return num;
+        });
+    }
+
+    default long readLong(String promt, String errorPrompt) {
+        return readObject(promt, errorPrompt, Long::parseLong);
+    }
+    default long readLong(String promt, String errorPrompt, long min, long max){
+        return readObject(String.format("%s","[%d-%d]", promt, min, max), errorPrompt, str -> {
+            long num = Long.parseLong(str);
+            if(num < min || num > max){
+                throw new RuntimeException(String.format("must be in the range [%d-%d]", min, max));
+            }
+            return num;
+        });
+    }
+    default LocalDate readDate(String promt, String errorPromt){
+        return readObject(promt, errorPromt, LocalDate::parse);
+    }
+    default LocalDate readDate(String prompt, String errorPrompt,LocalDate from, LocalDate to){
+        return readObject(prompt, errorPrompt, str -> {
+           LocalDate date = LocalDate.parse(str);
+           if(date.compareTo(from) < 0 || date.compareTo(to) > 0){
+               throw new RuntimeException(String.format("date must be in the range [%s -%s]", from, to));
+           }
+           return date;
+        });
+    }
+    default String readPredicate(String prompt, String errorPrompt, Predicate<String> predicate ){
+        //TODO
+        //returns string matching the given predicate
+        return null;
+    }
+    default String readOptions(String prompt, String errorPrompt, Set<String> options){
+        //TODO
+        //returns string included in the given
+        return null;
+    }
+    default double readDouble(String prompt, String errorPrompt){
+        return readObject(prompt, errorPrompt, Double::parseDouble);
+    }
+    default double readLong(String promt, String errorPrompt, double min, double max){
+        return readObject(String.format("%s","[%d-%d]", promt, min, max), errorPrompt, str -> {
+            double num = Double.parseDouble(str);
+            if(num < min || num > max){
+                throw new RuntimeException(String.format("must be in the range [%d-%d]", min, max));
+            }
+            return num;
+        });
+    }
 }
---
+++ src/telran/view/StandardInputOutput.java
@@ -0,0 +1,17 @@
+package telran.view;
+
+import java.util.Scanner;
+
+public class StandardInputOutput implements InputOutput{
+    private Scanner scanner = new Scanner(System.in);
+    @Override
+    public String readString(String prompt) {
+        writeLine(prompt);
+        return scanner.nextLine();
+    }
+
+    @Override
+    public void write(String str) {
+        System.out.println(str);
+    }
+}
